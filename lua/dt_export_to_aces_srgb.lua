22f39c99f93755aebd1b22ff1fcc7dd833d934a4
Vincent Girès <mail@vincentgires.com>
2020-03-12 23:06:35 +0100

    Add default target path

--- lua/dt_export_to_aces_srgb.lua
+++ lua/dt_export_to_aces_srgb.lua
@@ -4,7 +4,7 @@ local BLENDER_CONVERT_SCRIPT = os.getenv('BLENDER_USER_SCRIPTS')..'/templates_py
 
 local export_path = dt.new_widget('entry') {
   tooltip = 'target path to export file',
-  text = '',
+  text = dt.preferences.read('aces_export', 'export_path', 'string'),
   reset_callback = function(self) self.text = '' end
 }
 
@@ -17,9 +17,15 @@ end
 local function export_image(
     storage, image, format, filename,
     number, total, high_quality, extra_data)
-  print('exporting '..image.filename..' 'tostring(number)..'/'..tostring(total))
+  print('exporting '..image.filename..' '..tostring(number)..'/'..tostring(total))
   os.execute('blender --background --python '..BLENDER_CONVERT_SCRIPT..' -- -inputs '..filename..' -output '..export_path.text)
 end
+
+dt.preferences.register(
+  'aces_export', 'export_path',
+  'string', 'aces export: default export folderpath',
+  'default export location',
+  '/home/'..os.getenv('USER'))
 
 dt.register_storage('aces_export', 'export to aces srgb',
   export_image,
